ff0e13cdbd6c96f461c627b99ce2f1f16a6604af
Joanna Itzel Perez Ordaz <240222@utxicotepec.edu.mx>
2026-01-08 14:28:59 -0600

    crear funciones js y estilo del encabezado

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,30 @@
 function saludar(){
     alert("Bienvenido a la Academia de TI");
 }
+function suma(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 + valor2;
+    alert("Resultado de la suma de " +valor1+" y "+valor2+" es: " +resultado);
+}
+
+function resta(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 - valor2;
+    alert("Resultado de la resta de " +valor1+" y "+valor2+" es: " +resultado);
+}
+
+function mult(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 * valor2;
+    alert("Resultado de la multiplicacion de " +valor1+" y "+valor2+" es: " +resultado);
+}
+
+function div(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 / valor2;
+    alert("Resultado de la division de " +valor1+" y "+valor2+" es: " +resultado);
+}
